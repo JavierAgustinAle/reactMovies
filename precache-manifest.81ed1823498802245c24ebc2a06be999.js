156443aac93b0b3cfc0b5218abcd2c1f13d9dabb
JavierAle <javierale25@live.com>
2020-06-19 14:55:08 -0300

    Updates

---
+++ precache-manifest.81ed1823498802245c24ebc2a06be999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bd20c0722d41eeaab41144dd2bcfe3",
+    "url": "/reactMovies/index.html"
+  },
+  {
+    "revision": "2de7e7e741e6f36b7298",
+    "url": "/reactMovies/static/css/2.49f229d6.chunk.css"
+  },
+  {
+    "revision": "102261a16241a45cdfca",
+    "url": "/reactMovies/static/css/main.529b4cbd.chunk.css"
+  },
+  {
+    "revision": "2de7e7e741e6f36b7298",
+    "url": "/reactMovies/static/js/2.0ac2f6f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactMovies/static/js/2.0ac2f6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102261a16241a45cdfca",
+    "url": "/reactMovies/static/js/main.59a65a83.chunk.js"
+  },
+  {
+    "revision": "5827125363f6c1c4a6d5",
+    "url": "/reactMovies/static/js/runtime-main.7045351e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b89314958da6659bf2aa89fcbdb49dea.js"
+  "/reactMovies/precache-manifest.81ed1823498802245c24ebc2a06be999.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactMovies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
